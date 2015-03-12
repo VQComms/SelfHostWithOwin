6062c5cf7264b8a4c00d9006210aa464ce491f5c
Jonathan Channon <jonathan.channon@gmail.com>
2015-03-12 16:37:14 +0000

    initial commit

---
+++ SelfHostWithOwin/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Nancy;
+
+using Owin;
+using Nowin;
+using Microsoft.Owin.Builder;
+using System.Net;
+using Microsoft.Owin.Hosting;
+using System.Threading;
+
+namespace SelfHostWithOwin
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            var options = new StartOptions("http://127.0.0.1:1999")
+            {
+                ServerFactory = "Nowin",
+            };
+
+            using (WebApp.Start<Startup>(options))
+            {
+                Console.WriteLine("Running a http server on port 1999");
+                do
+                {
+                    Thread.Sleep(60000);
+                } while (!Console.KeyAvailable);
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hi";
+        }
+    }
+}
